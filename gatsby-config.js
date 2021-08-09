ce8afb8657157ffa29f626ae6183c404db1153c9
Ruixiang Du <ruixiang.du@gmail.com>
2021-08-09 23:02:35 +0800

    updated project base dir

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,7 +19,7 @@ module.exports = {
         configPath: `src/config`,
         docsPath: `src/docs`,
         repositoryUrl: `https://github.com/rxdu/note`,
-        baseDir: `examples/gatsby-theme-docs`,
+        baseDir: `/`,
       },
     },
     {
